4d4e3d06271a596ab774aa8a0ddb5d984dc280dd
morimolymoly <morimolymoly@gmail.com>
2018-07-12 17:36:05 +0900

    emulate wrmsr

--- src/trap.cpp
+++ src/trap.cpp
@@ -12,35 +12,103 @@ namespace mafia
 namespace intel_x64
 {
 
-void dump_regs(gsl::not_null<bfvmm::intel_x64::vmcs *> vmcs) noexcept
+::x64::msrs::value_type
+emulate_rdmsr(::x64::msrs::field_type msr)
 {
-    bferror_info(0, "dump regs");
-    bferror_subnhex(0, "rax", vmcs->save_state()->rax);
-    bferror_subnhex(0, "rbx", vmcs->save_state()->rbx);
-    bferror_subnhex(0, "rcx", vmcs->save_state()->rcx);
-    bferror_subnhex(0, "rdx", vmcs->save_state()->rdx);
-    bferror_subnhex(0, "rbp", vmcs->save_state()->rbp);
-    bferror_subnhex(0, "rsi", vmcs->save_state()->rsi);
-    bferror_subnhex(0, "rdi", vmcs->save_state()->rdi);
-    bferror_subnhex(0, "r08", vmcs->save_state()->r08);
-    bferror_subnhex(0, "r09", vmcs->save_state()->r09);
-    bferror_subnhex(0, "r10", vmcs->save_state()->r10);
-    bferror_subnhex(0, "r11", vmcs->save_state()->r11);
-    bferror_subnhex(0, "r12", vmcs->save_state()->r12);
-    bferror_subnhex(0, "r13", vmcs->save_state()->r13);
-    bferror_subnhex(0, "r14", vmcs->save_state()->r14);
-    bferror_subnhex(0, "r15", vmcs->save_state()->r15);
-    bferror_subnhex(0, "rip", vmcs->save_state()->rip);
-    bferror_subnhex(0, "rsp", vmcs->save_state()->rsp);
+    switch (msr) {
+        case ::intel_x64::msrs::ia32_debugctl::addr:
+            return ::intel_x64::vmcs::guest_ia32_debugctl::get();
+
+        case ::x64::msrs::ia32_pat::addr:
+            return ::intel_x64::vmcs::guest_ia32_pat::get();
+
+        case ::intel_x64::msrs::ia32_efer::addr:
+            return ::intel_x64::vmcs::guest_ia32_efer::get();
+
+        case ::intel_x64::msrs::ia32_perf_global_ctrl::addr:
+            return ::intel_x64::vmcs::guest_ia32_perf_global_ctrl::get_if_exists();
+
+        case ::intel_x64::msrs::ia32_sysenter_cs::addr:
+            return ::intel_x64::vmcs::guest_ia32_sysenter_cs::get();
+
+        case ::intel_x64::msrs::ia32_sysenter_esp::addr:
+            return ::intel_x64::vmcs::guest_ia32_sysenter_esp::get();
+
+        case ::intel_x64::msrs::ia32_sysenter_eip::addr:
+            return ::intel_x64::vmcs::guest_ia32_sysenter_eip::get();
+
+        case ::intel_x64::msrs::ia32_fs_base::addr:
+            return ::intel_x64::vmcs::guest_fs_base::get();
+
+        case ::intel_x64::msrs::ia32_gs_base::addr:
+            return ::intel_x64::vmcs::guest_gs_base::get();
+
+        default:
+            return ::intel_x64::msrs::get(msr);
+
+        // QUIRK:
+        //
+        // The following is specifically for CPU-Z. For whatever reason, it is
+        // reading the following undefined MSRs, which causes the system to
+        // freeze since attempting to read these MSRs in the exit handler
+        // will cause a GPF which is not being caught. The result is, the core
+        // that runs RDMSR on these freezes, the other cores receive an
+        // INIT signal to reset, and the system dies.
+        //
+
+        case 0x31:
+        case 0x39:
+        case 0x1ae:
+        case 0x1af:
+        case 0x602:
+            return 0;
+    }
 }
 
-static bool
-handle_syscall(gsl::not_null<bfvmm::intel_x64::vmcs *> vmcs)
+void
+emulate_wrmsr(::x64::msrs::field_type msr, ::x64::msrs::value_type val)
 {
+    switch (msr) {
+        case ::intel_x64::msrs::ia32_debugctl::addr:
+            ::intel_x64::vmcs::guest_ia32_debugctl::set(val);
+            return;
 
-    bfdebug_info(0, "[MAFIA] system call happend!");
-    dump_regs(vmcs);
-    return advance(vmcs);
+        case ::x64::msrs::ia32_pat::addr:
+            ::intel_x64::vmcs::guest_ia32_pat::set(val);
+            return;
+
+        case ::intel_x64::msrs::ia32_efer::addr:
+            ::intel_x64::vmcs::guest_ia32_efer::set(val);
+            return;
+
+        case ::intel_x64::msrs::ia32_perf_global_ctrl::addr:
+            ::intel_x64::vmcs::guest_ia32_perf_global_ctrl::set_if_exists(val);
+            return;
+
+        case ::intel_x64::msrs::ia32_sysenter_cs::addr:
+            ::intel_x64::vmcs::guest_ia32_sysenter_cs::set(val);
+            return;
+
+        case ::intel_x64::msrs::ia32_sysenter_esp::addr:
+            ::intel_x64::vmcs::guest_ia32_sysenter_esp::set(val);
+            return;
+
+        case ::intel_x64::msrs::ia32_sysenter_eip::addr:
+            ::intel_x64::vmcs::guest_ia32_sysenter_eip::set(val);
+            return;
+
+        case ::intel_x64::msrs::ia32_fs_base::addr:
+            ::intel_x64::vmcs::guest_fs_base::set(val);
+            return;
+
+        case ::intel_x64::msrs::ia32_gs_base::addr:
+            ::intel_x64::vmcs::guest_gs_base::set(val);
+            return;
+
+        default:
+            ::intel_x64::msrs::set(msr, val);
+            return;
+    }
 }
 
 class exit_handler_mafia : public bfvmm::intel_x64::exit_handler
@@ -51,10 +119,11 @@ public:
     {
         using namespace ::intel_x64::vmcs;
         bfdebug_info(0, "mafia hype you");
+        /*
         add_handler(
             exit_reason::basic_exit_reason::rdmsr,
             handler_delegate_t::create<mafia::intel_x64::handle_syscall>()
-        );
+        );*/
     }
     ~exit_handler_mafia() = default;
 };
